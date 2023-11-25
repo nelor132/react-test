6656686e2365d60d8ac8b25a02b9ef23717c48f4
nelor132 <65473161+nelor132@users.noreply.github.com>
2023-11-25 15:10:34 +0300

    Maga

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import axios from "axios";
+import { useState } from "react";
+import './App.css'
+
+
 function App() {
+  
+  const [state, setState] = useState("")
+  function handleWeather() {
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${state}&appid=b09c9aa03edba0cca4b144cc294a073a&units=metric`)
+    .then((response) => {
+      console.log(response)
+    } )
+    .finally(() => {
+      setState('')
+    })
+    }
   return (
     <div>
-      <h1> 123 </h1>
+      <input className="input" type="text" value={state} onChange={(pusher) => {
+        setState(pusher.target.value)
+      }}/>
+      <div className="div"></div>
+      <button onClick={handleWeather}> Поиск </button>
     </div>
   );
 }
-
 export default App;
+// b09c9aa03edba0cca4b144cc294a073a
